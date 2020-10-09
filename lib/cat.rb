3e8b4c86e3e394cf775c93c3fee4903dbe1b805b
Crystal Norman <cnorm88@gmail.com>
2020-10-09 15:53:02 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -4,12 +4,12 @@ class Cat
 
 @@all = []
   
-def initialize(name, owner)
-  @name = name
-  @owner = owner
-  @mood = "nervous"
-  @@all << self
-end
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
 
   def self.all
     @@all
