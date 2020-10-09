33c95e94e4b64fa98469e8d1e2fc886b4afbab70
Crystal Norman <cnorm88@gmail.com>
2020-10-09 14:59:40 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
 class Owner
-  # code goes here
+  
+attr_reader :name, :species
+attr_accessor :pets
+
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    @@pets = {:cats => [], :dogs => []}
+  end
+  
+   def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length
+  end    
+  
+  def self.reset_all
+    @@all = @@all.clear
+  end
+  
+  def say_species
+    "I am a #{@species}."
+  end
+  
+  def cats
+  end
+  
 end
